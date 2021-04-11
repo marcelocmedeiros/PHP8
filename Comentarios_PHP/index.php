44426caf74879103224d7e07852b98b5fbe9a9e7
Marcelo Campos de Medeiros <marcelocm31@gmail.com>
2021-04-11 18:38:02 -0300

    Estudando PHP 2 perido ADS

---
+++ Comentarios_PHP/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// COMENTÁRIOS
+// ------------
+
+// Comentário d euma linha 
+# Comentário d euma linha 
+
+/*
+Coméntario em 
+multiplas linhas
+*/
+
+echo 'Exemplo de comentário.';
+echo '<br>';//quebra linha na página HTML
+echo 'Ignora a instrução do comentário.';
